231b60e1ce6af88f5cedb35b02d48a4c1022dfa9
MarianaSko <maryana.skorynovych@gmail.com>
2023-12-14 13:19:04 +0200

    v-1

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -19,9 +19,9 @@ export const App = () => {
   const [number, setNumber] = useState('');
 
   useEffect(() => {
-    if (contacts?.length) {
-      setContacts(contacts);
-    }
+    // if (contacts?.length) {
+    //   setContacts(contacts);
+    // }
 
     localStorage.setItem('CONTACTS_DATA', JSON.stringify(contacts));
   }, [contacts]);
